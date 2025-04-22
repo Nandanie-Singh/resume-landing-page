d640631c3f5fa9328e662b03fba2cdd7d5bcf6ca
Nandanie-Singh <Nandanie.Singh@my.liu.edu>
2025-04-22 16:49:03 -0400

    fixed wrangler

--- .wrangler/tmp/dev-X7KovQ/index.js
+++ .wrangler/tmp/dev-X7KovQ/index.js
@@ -87,7 +87,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-5KZXXr/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-AdcC0Z/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -119,7 +119,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-5KZXXr/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-AdcC0Z/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
